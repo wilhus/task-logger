cddaf06f6e96826fd87dd8f877d1715323679d8b
William Husar <william.husar@hotmail.com>
2022-03-15 18:09:57 +0100

    Issue #1 - Created grid for tasks
    Tasks are now ordered in a grid that expands on the site depending on the amount of tasks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import { Grid, Stack, Typography } from '@mui/material'
 import { TaskCard } from '../components/TaskCard'
 import { Nav } from '../components/Nav'
 import { NewTask } from '../components/NewTask'
@@ -13,12 +11,73 @@ export default function Home() {
 
 	return (
 		<div>
-			<Nav />
-			<h1>To Do:</h1>
-			<NewTask />
-			{data?.map((task: TaskModel) => 
-				<TaskCard task={task} key={task.id}/>
-			)}
+			<div
+				style={{backgroundColor: 'green', 
+					padding:'10px', 
+					height:'10vh'
+				}}
+			>
+				<Nav
+				//style={{backgroundColor:'green', height:'10vh'}}
+				/>
+			</div>
+			<Stack
+				direction='column'
+				justifyContent='flex-start'
+				alignItems='center'
+				spacing={1}
+				style={{backgroundColor: 'grey', 
+					minHeight:'85vh'
+				}}
+			>
+				<Stack
+					direction='row'
+					alignItems='center'
+					spacing={100}
+					style={{paddingTop:'10px'}}
+				>
+					<Typography
+						variant='h4'
+					>
+						To Do
+					</Typography>
+					<NewTask />
+				</Stack>
+				<Grid
+					container
+					item
+					xs={10}
+					spacing={1}
+					rowSpacing={2}
+					style={{backgroundColor: 'red',
+						paddingBottom: '10px'
+					}}
+				>
+					{data?.map((task: TaskModel) =>
+						<Grid
+							item
+							xs={4}
+							key={task.id}
+						>
+							<TaskCard task={task} />
+						</Grid>
+					)}
+				</Grid>
+			</Stack>
+			<div
+				style={{backgroundColor:'lightblue', 
+					display:'flex', 
+					alignItems:'center',
+					justifyContent:'center',
+					height:'5vh'
+				}}
+			>
+				<Typography
+					variant='body2'
+				>
+					Made by wilhus @ <a href='https://github.com/wilhus' target='_blank'>GitHub</a>
+				</Typography>
+			</div>
 		</div>
 	)
 }
